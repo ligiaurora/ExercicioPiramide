4c1cc58832e49e0cd73b6d32dea41e553e077360
ligia <ligia@DESKTOP-33DDFM8>
2022-10-20 22:51:01 -0300

    ex piramide

---
+++ piramide/src/piramide/Main.java
@@ -0,0 +1,56 @@
+package piramide;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		Scanner entrada = new Scanner(System.in);
+		
+		Piramide p1 = new Piramide();
+		System.out.print("Informe a altura da piramide: ");
+		p1.setH(entrada.nextFloat());
+		
+		System.out.print("Informe AB da piramide: ");
+		p1.setAb(entrada.nextFloat());
+
+		System.out.print("Informe o tipo de tinta (1 - r$ 127,R$ 127,90; 2 – R$ 258,98; 3 – R$ 344,34): ");
+		p1.setTinta(entrada.nextInt());
+		
+		System.out.println("AB: "+p1.getAb());
+		System.out.println("H: "+p1.getH());
+		System.out.println("AL: "+p1.al());
+		System.out.println("Área triangulo: "+p1.ATriangulo());
+		System.out.println("Área base: "+p1.ABase());
+		System.out.println("Área Total: "+p1.ATotal());
+		System.out.println("Tipo da Tinta: "+p1.getTinta());
+		System.out.println("Litros: "+p1.Litros());
+		System.out.println("Quantidade de Latas: "+p1.Latas());
+		System.out.println("Preço: "+p1.Preco());
+		System.out.println("Volume: "+p1.Volume());
+		
+		
+		entrada.close();		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ piramide/src/piramide/Piramide.java
@@ -0,0 +1,68 @@
+package piramide;
+
+public class Piramide {
+
+	float h, ab;
+	double al, ATriangulo, bt, inter, at, litrosP, precoL, qtdLatas, vol;
+	int tinta;
+	
+	public float getH() {
+		return h;
+	}
+
+	public void setH(float h) {
+		this.h = h;
+	}
+
+	public float getAb() {
+		return ab;
+	}
+
+	public void setAb(float ab) {
+		this.ab = ab;
+	}
+
+	public int getTinta() {
+		return tinta;
+	}
+
+	public void setTinta(int tinta) {
+		this.tinta = tinta;
+	}				
+	
+
+	public double ATriangulo() {			
+		return ATriangulo = (ab * 2) * h / 2;
+	}
+	public double al() {			
+		return al = Math.sqrt(Math.pow(h,2)+Math.pow(ab,2));
+	}	
+	public double ABase() {			
+		return bt = Math.pow((ab)*2,2);
+	}
+	public double ATotal() {			
+		inter = (al * (ab * 2)) / 2;
+		return at = (inter * 4) + bt;
+	}
+	public double Litros() {						
+		return litrosP = at / 4.76;
+	}
+	public double Latas() {		
+		return Math.round(qtdLatas = litrosP / 18);	
+	}
+	
+	public double Preco() {											
+		
+		if (this.tinta == 1) {
+			return precoL = qtdLatas * 127.90;
+		} else if (this.tinta == 2) {
+			return precoL = qtdLatas * 258.98;
+		} else {
+			return precoL = qtdLatas * 344.34;
+		}
+	}
+	public double Volume() {		
+		return vol = (Math.pow(ab,2) * h) / 3;	
+	}
+
+}
